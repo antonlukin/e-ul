5c5e6600e27ff3da1fc15a016a1b2ddf7df36877
Anton Lukin <anton@lukin.me>
2017-09-15 15:32:50 +0300

    starter-kit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp      = require('gulp');
+var sass      = require('gulp-sass');
+var concat    = require('gulp-concat');
+var minifyCss = require('gulp-minify-css');
+var uglify    = require('gulp-uglify');
+var plumber   = require('gulp-plumber');
+var prefix    = require('gulp-autoprefixer');
+
+var path = {
+	source: 'src/',
+	assets: 'app/assets/'
+}
+
+gulp.task('scss', function() {
+    gulp.src([path.source + 'scss/app.scss'])
+        .pipe(plumber())
+        .pipe(sass({
+            errLogToConsole: true
+        }))
+		.pipe(prefix({
+			browsers: [
+				'ie >= 10',
+				'ie_mob >= 10',
+				'ff >= 30',
+				'chrome >= 34',
+				'safari >= 7',
+				'opera >= 23',
+				'ios >= 7',
+				'android >= 4.4',
+				'bb >= 10'
+			]
+		}))
+        .pipe(concat('styles.min.css'))
+        .pipe(minifyCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest(path.assets))
+})
+
+gulp.task('js', function() {
+    gulp.src([path.source + 'js/app.js'])
+        .pipe(plumber())
+		.pipe(uglify())
+        .pipe(concat('scripts.min.js'))
+        .pipe(gulp.dest(path.assets))
+})
+
+gulp.task('images', function() {
+	gulp.src([path.source + '/images/**/*'])
+		.pipe(gulp.dest(path.assets + '/images/'));
+})
+
+gulp.task('watch', function() {
+    gulp.watch(path.source + '/**/*', ['scss', 'js', 'images']);
+})
+
+gulp.task('default', ['js', 'scss', 'images', 'watch']);
