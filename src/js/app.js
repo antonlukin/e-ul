dc00d2a5f3e4d253439a7d42d6ce60075f2ad380
Anton Lukin <anton@lukin.me>
2017-09-19 13:27:51 +0300

    add simple mobile menu implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,8 @@
 (function(){
+  document.querySelector('.toggle').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    this.classList.toggle('toggle--expand');
+    document.querySelector('.menu').classList.toggle('menu--expand');
+  })
 })();
