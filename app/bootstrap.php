444cf3a9de22b22c53d53f33fec73dd9a764068f
Anton Lukin <anton@lukin.me>
2017-10-30 22:38:53 +0300

    add simple engine with flight

---
+++ app/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * E-ul: Ulyanovsk multifunctional center promo website
+ *
+ * @copyright   Copyright (c) 2017, Anton Lukin <anton@lukin.me>
+ * @license     MIT, https://github.com/antonlukin/e-ul/LICENSE
+ */
+
+
+/**
+ * Init app settings with .env
+ *
+ * @link https://github.com/vlucas/phpdotenv
+ * @since 1.0
+ */
+(new Dotenv\Dotenv(__DIR__ . '/../'))->load();
+
+
+/**
+ * Loading init helpers module
+ *
+ * @since 1.0
+ */
+(new app\helpers\core(__DIR__))->load();
+
+
--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
-	phpinfo();
+/**
+ * E-ul: Ulyanovsk multifunctional center promo website
+ *
+ * @copyright   Copyright (c) 2017, Anton Lukin <anton@lukin.me>
+ * @license     MIT, https://github.com/antonlukin/e-ul/LICENSE
+ */
+
+
+/* Register composer auto loader */
+require __DIR__ . '/../vendor/autoload.php';
+
+
+/* Get an app instance including bootstrap script  */
+require __DIR__ . '/../app/bootstrap.php';
